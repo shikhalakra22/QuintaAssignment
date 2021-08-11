f32d5660e83087c4a9e2b1f4299fab4c41458f02
Shikha Lakra <slakra567@gmail.com>
2021-08-11 10:50:26 +0530

    problem 5

---
+++ EmployeeMongodb/src/main/java/com/example/demo/EmployeeFunction.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+import java.util.*;
+import java.util.stream.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+@Service
+public class EmployeeFunction {
+	@Autowired
+	private EmployeeRepo repo;
+
+
+	public List<Employee> getAllEmployee() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	public List<Employee> getAllEmployeeRegion(String region) {
+		
+		return repo.findByRegion(region);
+	}
+
+	public List<Employee> findByName(String name) {
+		// TODO Auto-generated method stub
+		return repo.findByName(name);
+	}
+
+	public List<Employee> getAllbySalaryGreaterThan(int salary) {
+		// TODO Auto-generated method stub
+		
+		
+		return repo.findsalary(salary);
+	}
+
+	public List<Employee> findByNameStartingWith(String name) {
+		// TODO Auto-generated method stub
+		return repo.findByNameStartingWith(name);
+	}
+
+	
+	
+	
+
+}
